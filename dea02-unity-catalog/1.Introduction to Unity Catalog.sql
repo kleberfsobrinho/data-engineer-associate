10f390e8cb9c000b5abe1efe11c5e58f85e0ea31
kleberfsobrinho <kleberfsobrinho>
2025-04-16 20:10:22 +0000

    feat: introduction for Unity Catalog

---
+++ dea02-unity-catalog/1.Introduction to Unity Catalog.sql
@@ -0,0 +1,11 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC ### Check whether the workspace is attached to a Unity Catalog Metastore
+
+-- COMMAND ----------
+
+SELECT current_metastore();
+
+-- COMMAND ----------
+
+
